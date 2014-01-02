6cc73c24e80a527a0cee5bc2381ba9fc67f3933c
Lemuel Haham <lemuel@haham.net>
2014-01-01 19:34:47 -0600

    trying fb things

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,9 +2,6 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, "446246302141578", "c46e3008e98f6013bd72fecaf8d03bc0",
-    :scope => 'email,profile', :display => 'popup'
+    :scope => 'email,user_birthday,read_stream', :display => 'popup'
 
-    on_failure do
-      redirect_to root_path, notice: 'Refused...'      
-    end
 end

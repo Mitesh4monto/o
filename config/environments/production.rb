2e5df879bb8d53668aa1724e041c6fc8d6fea81a
Lemuel Haham <lemuel@haham.net>
2014-01-19 00:43:35 -0600

    ..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Opp::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( activities.css )
+  config.assets.precompile += [ 'activities.css', 'activities.js' )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false

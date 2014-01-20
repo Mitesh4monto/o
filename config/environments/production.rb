264d50ce9647ffd03088c960127bd4c553d62805
Lemuel Haham <lemuel@haham.net>
2014-01-20 02:23:00 -0600

    herok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,4 +68,16 @@ Opp::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.action_mailer.default_url_options = { :host => 'melearni.herokuapp.com' }
+  
+  config.action_mailer.delivery_method = :smtp 
+ 
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => "owlymacp@gmail.com",
+      :password             => "golfsmith5",
+      :authentication       => "plain",
+      :enable_starttls_auto => true
+    }
+  
 end

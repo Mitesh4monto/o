b2d6b667cb3f35002c624bc4499ba37221208159
Lemuel Haham <lemuel@haham.net>
2014-03-16 11:40:09 -0500

    fb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,7 @@ OmniAuth.config.on_failure = Proc.new { |env|
 }
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FB_APP_ID'],ENV['comment'],
+  provider :facebook, ENV['FB_APP_ID'],ENV['FB_APP_SECRET'],
     :scope => 'publish_stream, offline_access, email', :display => 'popup'
 
 end

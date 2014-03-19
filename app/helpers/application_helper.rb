4b481689f45c6de1c1f8c5e1bf381c8efdaa512d
Lemuel Haham <lemuel@haham.net>
2014-03-19 12:31:16 -0500

    add tips area - in progress

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
   def addcustomjs(files)
     content_for(:head_js) { javascript_include_tag(files) }
   end
+
+  # help tips with question mark image
+  def tipper(text, direction = 'bottom')
+    image_tag("/images/question_mark_blue.png", :class => 'button tipped', 'data-title' => text, 'data-tipper-options' => '{"direction":"' + direction + '"}', :height => '25') 
+  end
+  
 end

39274bacf168cf2928f2d5ce1013d09abf7576fb
Lemuel Haham <lemuel@haham.net>
2014-03-19 09:00:55 -0500

    visual modifs

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -32,6 +32,27 @@ $(function() {
 		var e="#Comments"+$(this)[0].id;
 		$(e).toggle(1000);
 	});
+
+	$('#link1').hover(function() { 
+	    $('#navmenu1').show(); 
+	}, function() { 
+	    $('#navmenu1').hide(); 
+	});
+	$('#link2').hover(function() { 
+	    $('#navmenu2').show(); 
+	}, function() { 
+	    $('#navmenu2').hide(); 
+	});
+	$('#link2a').hover(function() { 
+	    $('#navmenu2a').show(); 
+	}, function() { 
+	    $('#navmenu2a').hide(); 
+	});
+	$('#link2b').hover(function() { 
+	    $('#navmenu2b').show(); 
+	}, function() { 
+	    $('#navmenu2b').hide(); 
+	});
 	
 
 });

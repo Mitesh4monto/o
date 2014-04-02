36aa1805a70ab4f947ebb8d5e83c12cfef6d4ede
Lemuel Haham <lemuel@haham.net>
2014-04-02 17:48:58 -0500

    tweak styling, course join after signup, minor bug fixes, formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,18 @@ class ApplicationController < ActionController::Base
     @udpates = ActionLog.latest(current_user) if current_user
   end
   
+  def post_sign_in(user)
+    post_register(user) if (user.sign_in_count == 1)
+  end
+  
   def post_register(user)
+    puts '************************************************************'
     if session[:course_join] != nil      
       course = Course.find(session[:course_join])
       course.add_user_to_course(user)       
       session[:course_join] = nil
     else
+      puts '************************************************************'
       course = Course.find(AppSetting.get('intro_course'))
       course.add_user_to_course(user)       
     end    
@@ -27,5 +33,28 @@ class ApplicationController < ActionController::Base
         end
       end
   end
+  
+  protected
+  def after_update_path_for(resource)
+      my_profile_path
+  end
+      
+  def after_sign_in_path_for(resource)
+    post_sign_in(resource)
+    root_path
+  end
+
+  def after_sign_up_path_for(resource)
+    puts '[****************************************][[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]'
+      post_register(resource)    
+      root_path
+  end
+  
+  def after_inactive_sign_up_path_for(resource)
+    puts '****************************************[[[[[[[[[[[[]]]]]]]]]]]]'
+    post_register(resource)    
+    root_path
+  end
+  
   
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,35 @@
 class RegistrationsController < Devise::RegistrationsController
+  
+  
   protected
+  
+  def new
+    puts '****************************************'    
+  end
+  
+  def create
+    puts '****************************************'    
+  end
+
+  puts '****************************************[[[[[[[]]]]]]]'
+
+
+  def after_sign_in_path_for(resource)
+    puts '****************************************[[[[[[[]]]]]]]'    
+  end
+
+  def after_sign_out_path_for(resource)
+    puts '****************************************[[[[[[[]]]]]]]'
+  end
 
   def after_sign_up_path_for(resource)
+    puts '****************************************[[[[[[[]]]]]]]'
       post_register(resource)    
       root_path
   end
   
   def after_inactive_sign_up_path_for(resource)
+    puts '****************************************[[[[[[[]]]]]]]'
     post_register(resource)    
     root_path
   end
